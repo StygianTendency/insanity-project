236248dddf24229cfff2a72842187b1980c37bc1
David T. <92059858+StygianTendency@users.noreply.github.com>
2024-05-07 22:53:56 -0500

    Screen Shake Working

--- Insanity Project/Assets/ScreenShake.cs
+++ Insanity Project/Assets/ScreenShake.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using System.Collections;
 public class ScreenShake : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float shakeDuration = 0.1f;
+    public float shakeIntensity = 0.1f;
+
+    private Vector3 originalPosition;
+
     void Start()
     {
-        
+        // Store the original position of the camera
+        originalPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Shake()
     {
-        
+        // Start the screen shake coroutine
+        StartCoroutine(ShakeCoroutine());
+    }
+
+    IEnumerator ShakeCoroutine()
+    {
+        float elapsed = 0.0f;
+
+        while (elapsed < shakeDuration)
+        {
+            // Generate random offsets for the camera position
+            float xOffset = Random.Range(-1f, 1f) * shakeIntensity;
+            float yOffset = Random.Range(-1f, 1f) * shakeIntensity;
+
+            // Apply the offset to the camera's position
+            transform.position = originalPosition + new Vector3(xOffset, yOffset, 0);
+
+            // Increment the elapsed time
+            elapsed += Time.deltaTime;
+
+            // Wait for the next frame
+            yield return null;
+        }
+
+        // Reset the camera's position to its original position
+        transform.position = originalPosition;
     }
 }
+
